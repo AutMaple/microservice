b3619e9f52e27fe31d33a5ef9795541276fc3643
AutMaple <838498279@qq.com>
2023-05-04 18:08:36 +0800

    准备为网关添加安全配置

--- gateway/src/main/java/com/autmaple/config/SecurityConfig.java
+++ gateway/src/main/java/com/autmaple/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.autmaple.config;
 
+import com.autmaple.common.config.KeycloakJwtAuthenticationConverter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.server.resource.web.server.ServerBearerTokenAuthenticationConverter;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class SecurityConfig {
@@ -17,14 +19,20 @@ public class SecurityConfig {
                 .anyExchange().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .bearerTokenConverter(new ServerBearerTokenAuthenticationConverter())
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(jwt -> Mono.fromSupplier(() -> new KeycloakJwtAuthenticationConverter().convert(jwt)));
         return httpSecurity.build();
     }
 
     @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder) {
         // todo need to modify
+        return null;
+    }
+
+    @Bean
+    ReactiveJwtDecoder reactiveJwtDecoder() {
+        // todo add a ReactiveJwtDecoder
         return null;
     }
 }
